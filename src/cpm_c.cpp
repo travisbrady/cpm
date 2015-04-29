3d222e4b94b879135ff397557fc50098c89dc2d8
Travis Brady <travis.brady@gmail.com>
2015-04-28 21:57:01 -0500

    add dataset functions

--- src/cpm_c.cpp
+++ src/cpm_c.cpp
@@ -20,8 +20,6 @@ extern "C" {
     
     //void predict(const StochasticDataAdaptor& testset, float* scores, int* assignments) const;
     void predict(cpm_model c_model, float* data, int* labels, size_t n_instances, size_t n_dimensions, float* scores, int* assignments) {
-        printf("[predict] assignments: %d %d\n", assignments[0], assignments[1]);
-        printf("[predict] labels: %d %d\n", labels[0], labels[1]);
         CPM* model = (CPM*)c_model;
         StochasticDataAdaptor sda = StochasticDataAdaptor(data, labels, n_instances, n_dimensions);
         model->predict(sda, scores, assignments);
@@ -34,6 +32,10 @@ extern "C" {
         model->serializeModel(filename);
     }
 
+    cpm_model deserializeModel(const char* filename) {
+        return (cpm_model)CPM::deserializeModel(filename);
+    }
+
     int get_outer_label(cpm_model c_model) {
         CPM* model = (CPM*)c_model;
         return model->outer_label;
@@ -42,5 +44,25 @@ extern "C" {
     int get_k(cpm_model c_model) {
         CPM* model = (CPM*)c_model;
         return model->k;
+    }
+
+    cpm_dataset cpm_dataset_from_file(const char* fname, size_t n_instances) {
+        StochasticDataAdaptor* sda = new StochasticDataAdaptor::StochasticDataAdaptor(fname, n_instances);
+        //StochasticDataAdaptor* p_sda = static_cast<StochasticDataAdaptor*>(&sda);
+        printf("Ninst: %lu\n", sda->getNInstances());
+        //return static_cast<cpm_dataset>(p_sda);
+        return (cpm_dataset)sda;
+    }
+
+    cpm_dataset cpm_dataset_from_dense_memory(float* data, int* labels, size_t n_instances, size_t n_dimensions) {
+        StochasticDataAdaptor sda = StochasticDataAdaptor(data, labels, n_instances, n_dimensions);
+        StochasticDataAdaptor* p_sda = static_cast<StochasticDataAdaptor*>(&sda);
+        return static_cast<cpm_dataset>(p_sda);
+    }
+
+    size_t cpm_dataset_get_n_instances(cpm_dataset c_ds) {
+        StochasticDataAdaptor* p_ds = (StochasticDataAdaptor*)c_ds;
+        printf("Ninst: %lu\n", p_ds->getNInstances());
+        return p_ds->getNInstances();
     }
 }
--- src/cpm_c.h
+++ src/cpm_c.h
@@ -5,12 +5,17 @@ extern "C" {
 #endif
 
     typedef void * cpm_model;
+    typedef void * cpm_dataset;
     cpm_model create(void);
     void fit(cpm_model, float*, int*, size_t, size_t, int, bool, bool);
     void predict(cpm_model, float*, int*, size_t, size_t, float*, int*);
     void serializeModel(cpm_model, const char*);
+    cpm_model deserializeModel(const char* filename);
     int get_outer_label(cpm_model);
     int get_k(cpm_model);
+    cpm_dataset cpm_dataset_from_file(const char*, size_t);
+    cpm_dataset cpm_dataset_from_dense_memory(float*, int*, size_t, size_t);
+    size_t cpm_dataset_get_n_instances(cpm_dataset);
 
 #ifdef __cplusplus
 }
